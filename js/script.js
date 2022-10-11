a0a23378e20dd2997b208536b20c87ba04837f25
Omar-Codess <omartaki8@gmail.com>
2022-10-11 17:44:45 +0200

    pseudocodice esercizio dadi

--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
-const emails = ["olga@email.com", "muaro@email.com", "marco@email.com"]
-console.log(emails);
+// const emails = ["olga@email.com", "muaro@email.com", "marco@email.com"]
+// console.log(emails);
 
-userEmail = prompt("Inserisci la tua email")
-console.log(userEmail);
+// userEmail = prompt("Inserisci la tua email")
+// console.log(userEmail);
 
-let isFound = false;
+// let isFound = false;
 
-for (let i = 0; i < emails.length; i++) {
-    const thisEmail = emails[i];
+// for (let i = 0; i < emails.length; i++) {
+//     const thisEmail = emails[i];
 
-    if (thisEmail === userEmail) {
-        isFound = true;
-    }     
-}
+//     if (thisEmail === userEmail) {
+//         isFound = true;
+//     }     
+// }
 
-if (isFound) {
-    alert("Email corretta!")
-} else {
-    alert("Email errata.")
-}
+// if (isFound) {
+//     alert("Email corretta!")
+// } else {
+//     alert("Email errata.")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Chiedere all'utente di inserire un numero tra 1 e 6
+// Far generare al computer un numero casuale tra 1 e 6
+// SE il numero dell'utente è maggiore del numero del computer
+    // scrivi hai vinto!
+// ALTRIMENTI SE il numero generato dall'utente è inferiore 
+    // scrivi hai perso!

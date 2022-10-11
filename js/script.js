e4dfc6d14f78b245a32489c401a140f30ce7e891
Omar-Codess <omartaki8@gmail.com>
2022-10-11 17:01:46 +0200

    aggiunte e testate condizioni esercizio email

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Chiedi all'utente la sua email
-// SE è nella lista
-    // scrivi email corretta
-// ALTRIMENTI SE non è nella lista
-    // scrivi email errata
+const emails = ["olga@email.com", "muaro@email.com", "marco@email.com"]
+console.log(emails);
+
+userEmail = prompt("Inserisci la tua email")
+console.log(userEmail);
+
+let isFound = false;
+
+for (let i = 0; i < emails.length; i++) {
+    const thisEmail = emails[i];
+
+    if (thisEmail === userEmail) {
+        isFound = true;
+    }     
+}
+
+if (isFound) {
+    alert("Email corretta!")
+} else {
+    alert("Email errata.")
+}
